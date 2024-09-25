06835adf5f8085fe4608fd59aa02d06cf4b508a3
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2024-09-25 10:03:12 +0500

    Update task1.py

--- lab 6/task1.py
+++ lab 6/task1.py
@@ -1,17 +1,12 @@
-def count(text1):
-    try:
-        with open(r'text1.txt') as fileObj:
-            content = fileObj.read()
-            characters = len(content)
-            words = len(content.split())
-            print("Total characters: ", characters)
-            print("Total words: ", words)
-    except FileNotFoundError:
-        print("File not found.")
-    except IOError:
-        print("Cannot read file.")
-    except Exception as e:
-        print("An unexpected error occurred: ", e)
+import pandas as pd
 
+data = {
+    'title': ['Harry Potter and the Prisoner of Azkaban', 'Interstellar', 'The Notebook', 'Home Alone 3'],
+    'revenue': [2345678, 567890, 340909098, 3456789],
+    'spent': [890678, 2345679, 456789, 6789087]
+}
 
-count('text.txt')
+df = pd.DataFrame(data)
+
+filter = df[(df['revenue'] > 2000000) & (df['spent'] < 1000000)]
+print(filter)
