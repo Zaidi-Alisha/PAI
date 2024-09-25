de1c81938d3d094e25e61ef1feaa8ba474d42e7f
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2024-09-25 10:03:33 +0500

    Update task2.py

--- lab 6/task2.py
+++ lab 6/task2.py
@@ -1,19 +1,11 @@
-def search_and_replace(text2, search_word, replace_word):
-    try:
-       with open(r'text2.txt') as fileObj:
-            content = fileObj.read()
-            newContent = content.replace(search_word, replace_word)
+import pandas as pd
 
-            with open(r'text2.txt','w') as fileObj:
-                fileObj.write("Hello Alisha")
+data = {
+    'title': ['Harry Potter and the Prisoner of Azkaban', 'Interstellar', 'The Notebook', 'Home Alone 3'],
+    'runtime': [120, 90, 150, 85]
+}
 
-                print("All occurrences of ", search_word, "replaced with ", replace_word)
+df = pd.DataFrame(data)
 
-    except FileNotFoundError:
-        print("Error: File not found.")
-    except IOError:
-        print("Error: Unable to read or write to the file.")
-    except Exception as e:
-        print("An unexpected error occurred: ", e)
-
-search_and_replace('text2.txt', 'hello', 'hi')
+sorted = df.sort_values(by='runtime', ascending=False)
+print(sorted)
