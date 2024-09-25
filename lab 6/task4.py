ffaf1fa0c894cb8f31ae45fe82ee4fc5bbe1dd4f
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2024-09-25 10:04:20 +0500

    Update task4.py

--- lab 6/task4.py
+++ lab 6/task4.py
@@ -1,32 +1,13 @@
-def save_biodata(text4):
-    try:
-        name = input("Enter Name: ")
-        cnic = input("Enter CNIC Number: ")
-        age = input("Enter Age: ")
-        salary = input("Enter Salary: ")
+import pandas as pd
 
-        with open("text4.txt", 'w') as fileObj:
-            fileObj.write(f"Name: {name}\n")
-            fileObj.write(f"CNIC: {cnic}\n")
-            fileObj.write(f"Age: {age}\n")
-            fileObj.write(f"Salary: {salary}\n")
+data = {
+    'name': ['Alisha','Layyana','Amna','Laiba'],
+    'age': [23, 29, 13, 26]
+}
+df = pd.DataFrame(data)
 
-        print("Biodata saved.")
+change = [34, 35, 36, 37]
 
-        contact = input("Enter Contact Number: ")
-        with open(text4, 'a') as fileObj:
-            fileObj.write(f"Contact Number: {contact}\n")
-
-        print("Contact number changed.")
-        with open(text4, 'r') as fileObj:
-            content = fileObj.read()
-            print("File content:")
-            print(content)
-
-    except IOError:
-        print("Error: Unable to write to or read the file.")
-    except Exception as e:
-        print("An unexpected error occurred: ", e)
-
-
-save_biodata('text4.txt')
+df.index = change
+print("\nDataFrame with custom index:")
+print(df)
