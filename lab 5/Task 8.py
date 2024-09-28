2ac724a6c750db47af06afec0811fde38fbb3be4
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2024-09-28 16:54:57 +0500

    Update Task 8.py

--- lab 5/Task 8.py
+++ lab 5/Task 8.py
@@ -1,9 +1,7 @@
 import re
 
-text = """
-We have several dates in this text: 12/09/2023, 2023-09-12, and Sep 12, 2023. 
-Another date is 01/01/2022.
-"""
+text = "We have several dates in this text: 12/09/2023, 2023-09-12, and Sep 12, 2023. 
+Another date is 01/01/2022"
 
 # Regular expression to capture various date formats
 date_pattern = r'(\d{2}/\d{2}/\d{4})|(\d{4}-\d{2}-\d{2})|([A-Za-z]{3} \d{2}, \d{4})'
