34cbb16e44aa83c4ce0c9238d4eea6fc92cbf15b
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2024-10-27 18:00:42 +0500

    Update task 8.py

--- lab 10/task 8.py
+++ lab 10/task 8.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
-x = np.arange(1, 11)
-y1 = x ** 2  # Example metric 1
-y2 = x ** 1.5  # Example metric 2
 
-plt.figure(figsize=(10, 6))
+x = np.arange(1, 11)
+y1 = x ** 2  
+y2 = x ** 1.5  
 plt.plot(x, y1, color='pink', marker='o', label='y1 = x^2')
 plt.plot(x, y2, color='gray', marker='o', label='y2 = x^1.5')
 
