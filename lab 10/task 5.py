b7f9ce6da641d15f527041caef2f8079ee45ec4d
Zaidi-Alisha <142868085+Zaidi-Alisha@users.noreply.github.com>
2024-10-27 17:59:14 +0500

    Update task 5.py

--- lab 10/task 5.py
+++ lab 10/task 5.py
@@ -1,10 +1,10 @@
+import pandas as pd
 import matplotlib.pyplot as plt
-# Sample gender data
-genders = ['Male'] * 12 + ['Female'] * 8  # 12 Male, 8 Female
-gender_distribution = [genders.count('Male'), genders.count('Female')]
 
-plt.figure(figsize=(7, 7))
-plt.pie(gender_distribution, labels=['Male', 'Female'], autopct='%1.1f%%', startangle=140)
-plt.title('Gender Distribution Among Students')
-plt.axis('equal')
+data = pd.read_csv('students.csv')
+
+gender = data['Gender'].value_counts()
+
+plt.pie(gender, labels=gender.index, autopct='%1.1f%%', startangle=140)
+plt.title('Gender Distribution')
 plt.show()
